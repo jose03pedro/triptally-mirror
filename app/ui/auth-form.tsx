49a2226305b0013a5557c05a4b4bcc94fda22d91
marianamarquess <marianamarques1217@gmail.com>
2025-10-27 12:38:02 +0000

    Reusable component auth-form for signup and login

--- app/ui/auth-form.tsx
+++ app/ui/auth-form.tsx
@@ -4,11 +4,12 @@ import { useActionState, useState } from "react";
 import { signup } from "../actions/signup";
 
 type AuthFormProps = {
-  submitType: string;
+  mode: "login" | "signup";
 };
 
-export default function AuthForm({ submitType }: AuthFormProps) {
-  const [state, action, pending] = useActionState(signup, undefined);
+export default function AuthForm({ mode }: AuthFormProps) {
+  const actionFn = signup;
+  const [state, action, pending] = useActionState(actionFn, undefined);
 
   const [formValues, setFormValues] = useState({
     email: "",
@@ -27,7 +28,7 @@ export default function AuthForm({ submitType }: AuthFormProps) {
       <header>
         <h2 className="h5 mb-1">Your smart travel planner.</h2>
         <h3 className="h5 text-secondary">
-          {submitType} your TripTally account
+          {mode === "login" ? "Login" : "Create"} your TripTally account
         </h3>
       </header>
 
@@ -58,6 +59,7 @@ export default function AuthForm({ submitType }: AuthFormProps) {
               </div>
             ))}
         </div>
+
         {/* Password Field */}
         <div className="mb-3">
           <label
@@ -90,59 +92,65 @@ export default function AuthForm({ submitType }: AuthFormProps) {
             </div>
           )}
         </div>
-        {/* First Name Field */}
-        <div className="mb-2">
-          <label
-            htmlFor="first_name"
-            className="form-label text-secondary mb-0 fs-7"
-          >
-            First Name
-          </label>
-          <input
-            id="first_name"
-            name="first_name"
-            type="text"
-            placeholder="Enter your first name..."
-            value={formValues.first_name}
-            onChange={handleChange}
-            className={`form-control fs-6 ${
-              state?.errors?.first_name ? "is-invalid" : ""
-            }`}
-          />
-          {state?.errors?.first_name &&
-            state.errors.first_name.map((err, i) => (
-              <div key={i} className="invalid-feedback">
-                {err}
-              </div>
-            ))}
-        </div>
 
-        {/* Last Name Field */}
-        <div className="mb-2">
-          <label
-            htmlFor="last_name"
-            className="form-label text-secondary mb-0 fs-7"
-          >
-            Last Name
-          </label>
-          <input
-            id="last_name"
-            name="last_name"
-            type="text"
-            placeholder="Enter your last name..."
-            value={formValues.last_name}
-            onChange={handleChange}
-            className={`form-control fs-6 ${
-              state?.errors?.last_name ? "is-invalid" : ""
-            }`}
-          />
-          {state?.errors?.last_name &&
-            state.errors.last_name.map((err, i) => (
-              <div key={i} className="invalid-feedback">
-                {err}
-              </div>
-            ))}
-        </div>
+        {/* Extra fields for signup */}
+        {mode === "signup" && (
+          <div>
+            {/* First Name Field */}
+            <div className="mb-2">
+              <label
+                htmlFor="first_name"
+                className="form-label text-secondary mb-0 fs-7"
+              >
+                First Name
+              </label>
+              <input
+                id="first_name"
+                name="first_name"
+                type="text"
+                placeholder="Enter your first name..."
+                value={formValues.first_name}
+                onChange={handleChange}
+                className={`form-control fs-6 ${
+                  state?.errors?.first_name ? "is-invalid" : ""
+                }`}
+              />
+              {state?.errors?.first_name &&
+                state.errors.first_name.map((err, i) => (
+                  <div key={i} className="invalid-feedback">
+                    {err}
+                  </div>
+                ))}
+            </div>
+
+            {/* Last Name Field */}
+            <div className="mb-2">
+              <label
+                htmlFor="last_name"
+                className="form-label text-secondary mb-0 fs-7"
+              >
+                Last Name
+              </label>
+              <input
+                id="last_name"
+                name="last_name"
+                type="text"
+                placeholder="Enter your last name..."
+                value={formValues.last_name}
+                onChange={handleChange}
+                className={`form-control fs-6 ${
+                  state?.errors?.last_name ? "is-invalid" : ""
+                }`}
+              />
+              {state?.errors?.last_name &&
+                state.errors.last_name.map((err, i) => (
+                  <div key={i} className="invalid-feedback">
+                    {err}
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
       </div>
 
       <button type="submit" className="btn btn-primary w-100">
