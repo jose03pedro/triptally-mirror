e4215163f608c8c85c1ee943f108756c98d6c9e2
marianamarquess <marianamarques1217@gmail.com>
2025-10-27 16:58:51 +0000

    Init tests for signup feature

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+  },
+};
