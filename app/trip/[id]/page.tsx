432072c49f0108e4bd1e61aa7adceb4a74183f49
Mariana Marques <marianamarques1217@gmail.com>
2025-10-29 22:10:27 +0000

    app builds locally with success

--- app/trip/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface TripProps {
-    params: { id: string };
-}
-
-export default function TripPage({ params }: TripProps) {
-    return <div>Trip ID: {params.id}</div>;
-}
