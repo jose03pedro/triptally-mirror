e83afe6265548fdc684f01654137b55af6b2e0e3
Zsolten <nistorzsolt5@gmail.com>
2025-12-19 11:18:38 +0000

    bugfix

--- app/api/traveler/[id]/route.ts
+++ app/api/traveler/[id]/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(
     request: NextRequest,
     context: { params: Promise<{ id: string }> }
-
+){
     try {
         await connectionToDB();
         const { id } = await context.params;
